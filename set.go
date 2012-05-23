85d1e7be6d295b193e6015b5f2f4e43ec85f4f9c
slowpoke <proxypoke@lavabit.com>
2012-05-23 15:39:26 +0000

    Package and set.Interface documentation.

--- set.go
+++ set.go
@@ -3,10 +3,29 @@
 // Do What The Fuck You Want To Public License, 
 // found in the COPYING file or at http://sam.zoy.org/wtfpl/COPYING
 
-// TODO: documentation for package and functions
-
+// Package set provides set operations for any type implementing the
+// set.Interface. A set is an unordered collection of non-repeating values
+// which usually has constant time for adding, removing and querying
+// membership of element. They also support powerful set-theoretical operations
+// such as union and intersection.
+//
+// This interface doesn't guarantee these times, though, as it depends on the
+// underlying type implementing it. A good candidate would be, for example, a
+// map[T]bool, where T are the elements in the set and the boolean value
+// indicating whether an element is part of the set.
+//
+// Sets can be used anywhere duplicate values would not make sense and fast
+// membership tests are desired, for example a tagging mechanism or a friend
+// list. 
 package set
 
+// If a type implements set.Interface, it can be used as a set. It's preferable
+// for these methods to have constant time complexity, but this isn't
+// necessary.
+// 
+// NOTE: All of these methods are meant for this package to call them. Use the
+// appropriate function in this package instead (eg. don't use
+// type.Add(x), use set.Add(type, x))
 type Interface interface {
 	Add(x interface{}) bool
 	Discard(x interface{}) bool
